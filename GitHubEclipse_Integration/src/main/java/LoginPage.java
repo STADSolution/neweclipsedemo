628f1638725a2cb370c2747de7563998c42418e1
91978 <91978@STADSOLUTION>
2021-12-17 18:48:07 +0530

    First Commit

---
+++ GitHubEclipse_Integration/src/main/java/LoginPage.java
@@ -0,0 +1,10 @@
+
+
+public class LoginPage {
+
+	public static void main(String[] args) {
+	System.out.println("Hello in login Page");
+	System.out.println("This is new line");
+	}
+
+}
---
+++ GitHubEclipse_Integration/src/main/java/jenkien/GitHubEclipse_Integration/App.java
@@ -0,0 +1,13 @@
+package jenkien.GitHubEclipse_Integration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
