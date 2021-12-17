43667b13c965b0da3a8be978e48101ec3fac88f2
91978 <91978@STADSOLUTION>
2021-12-17 19:34:42 +0530

    Second Commit

--- GitHubEclipse_Integration/src/main/java/LoginPage.java
+++ GitHubEclipse_Integration/src/main/java/LoginPage.java
@@ -5,6 +5,7 @@ public class LoginPage {
 	public static void main(String[] args) {
 	System.out.println("Hello in login Page");
 	System.out.println("This is new line");
+	System.out.println("This is eclipse changes");
 	}
 
 }
